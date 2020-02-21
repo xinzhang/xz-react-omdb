b8f34d2229777b8ccb78b152eadac4798528fe98
Xin Zhang <xin.zhang@sca.com.au>
2020-02-21 14:04:50 +1100

    init draft

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/health', (req, res) => {
+  res.send('health check: OK');
+});
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+app.listen(port, () =>
+  console.log(
+    `${process.env.REACT_APP_PRODUCT_NAME} app is listending on port ${port}`,
+  ),
+);
