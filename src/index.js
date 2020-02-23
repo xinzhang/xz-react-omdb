4cd589678fd414c505ad1a9226d3916b5b18c38e
Xin Zhang <xin.zhang@sca.com.au>
2020-02-24 10:07:10 +1100

    remove redux store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import App from './App';
-import store from './store';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </ThemeProvider>,
   document.getElementById('root'),
 );
